380668968e9bdb51f179bf46523e1fe10de82617
Pablomenma <pablostm30@gmail.com>
2023-10-14 18:57:32 +0200

    agrego una tercera fila con mi comida favorita

--- prueba.java
+++ prueba.java
@@ -2,6 +2,7 @@ public class  prueba{
 	public static void main(String args []) {
 	System.out.println("Mendoza Mayo");
     System.out.println("24, 1998");
+    System.out.println("parrillada de verduras ");
 }
 
 }
